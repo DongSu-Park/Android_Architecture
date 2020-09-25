cd9a92214073eac367aa4a53d478e3c7993e8f9f
FloreErin <floreerin@gmail.com>
2020-09-25 10:15:33 +0900

    2nd Presenter Detach

--- app/src/main/java/com/example/architecture_tutorial/Presenter/MainPresenter.kt
+++ app/src/main/java/com/example/architecture_tutorial/Presenter/MainPresenter.kt
@@ -1,37 +1,61 @@
 package com.example.architecture_tutorial.Presenter
 
 import android.content.Context
-import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.architecture_tutorial.Model.Item
 import com.example.architecture_tutorial.Model.ItemDatabase
+import com.example.architecture_tutorial.R
+import com.example.architecture_tutorial.UI.MainActivity
+import com.example.architecture_tutorial.adapter.ItemAdapter
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainPresenter() : Presenter {
-
+class MainPresenter(private val view: MainActivity) : Presenter {
     lateinit var items : List<Item>
 
-    override fun loadDB(context : Context): List<Item> {
+    override fun loadDB(context : Context) {
         val db : ItemDatabase = ItemDatabase.getInstance(context)
         items = db.itemDao().getAll()
-        return items
+        view.layout_recyclerView.run{
+            adapter = ItemAdapter(context, items)
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+        }
     }
 
-    override fun addDB(context : Context, newId: Int?, newMessage: String): List<Item> {
+    override fun addDB(context : Context) {
         val db : ItemDatabase = ItemDatabase.getInstance(context)
+        val newId : Int? = null
+        val newMessage : String = view.et_input.text.toString()
+
         val newItem = Item(newId, newMessage)
 
         db.itemDao().insert(newItem)
         items = db.itemDao().getAll()
-
-        return items
+        view.layout_recyclerView.run {
+            adapter = ItemAdapter(context, items)
+            adapter?.notifyDataSetChanged()
+        }
     }
 
-    override fun clearDB(context : Context): List<Item> {
+    override fun clearDB(context : Context) {
         val db : ItemDatabase = ItemDatabase.getInstance(context)
         db.itemDao().allDelete()
         items = db.itemDao().getAll()
 
-        return items
+        val builder = MaterialAlertDialogBuilder(context, R.style.CustomMaterialDialog)
+        builder.run {
+            setTitle("삭제 메세지")
+            setMessage("전체 메세지를 삭제하겠습니까?")
+            setPositiveButton("확인") { dialog, width ->
+                view.layout_recyclerView.run{
+                    adapter = ItemAdapter(context, items)
+                    adapter?.notifyDataSetChanged()
+                }
+            }
+            setNegativeButton("취소") { dialog, width -> }
+            show()
+        }
     }
 
 }
--- app/src/main/java/com/example/architecture_tutorial/UI/MainActivity.kt
+++ app/src/main/java/com/example/architecture_tutorial/UI/MainActivity.kt
@@ -2,50 +2,28 @@ package com.example.architecture_tutorial.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.architecture_tutorial.Presenter.MainPresenter
 import com.example.architecture_tutorial.R
-import com.example.architecture_tutorial.adapter.ItemAdapter
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var presenter = MainPresenter()
+    var presenter = MainPresenter(this@MainActivity)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Layout Setting
-        layout_recyclerView.run{
-            adapter = ItemAdapter(context, presenter.loadDB(context))
-            layoutManager = LinearLayoutManager(context)
-            setHasFixedSize(true)
-        }
+        presenter.loadDB(this@MainActivity)
 
         // Input Button Click Event
         btn_input.setOnClickListener {
-            val newId : Int? = null
-            val newMessage : String = et_input.text.toString()
-
-            layout_recyclerView.adapter = ItemAdapter(this, presenter.addDB(this@MainActivity, newId, newMessage))
-            layout_recyclerView.adapter?.notifyDataSetChanged()
+            presenter.addDB(this@MainActivity)
         }
 
         // List Clear Button Click Event
         btn_clear.setOnClickListener {
-            val builder = MaterialAlertDialogBuilder(this, R.style.CustomMaterialDialog)
-            builder.run {
-                setTitle("삭제 메세지")
-                setMessage("전체 메세지를 삭제하겠습니까?")
-                setPositiveButton("확인") { dialog, width ->
-
-                    layout_recyclerView.adapter = ItemAdapter(context, presenter.clearDB(context))
-                    layout_recyclerView.adapter?.notifyDataSetChanged()
-                }
-                setNegativeButton("취소") { dialog, width -> }
-                show()
-            }
+           presenter.clearDB(this@MainActivity)
         }
     }
 }
